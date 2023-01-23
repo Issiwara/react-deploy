4a4b5cc2fe2f81d0b06605941397d7703e926645
ISSIWARA KAVINDA <65284203+Issiwara@users.noreply.github.com>
2023-01-23 19:14:50 +0530

    v02

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CICD pipeline using Github Actions.
+          demo CI/CD pipeline using Github Actions.
         </p>
         <a
           className="App-link"
