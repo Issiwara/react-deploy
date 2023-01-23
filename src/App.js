1d1fdb1fc18ed8d5b6b6e9810f8153c236f757dd
ISSIWARA KAVINDA <65284203+Issiwara@users.noreply.github.com>
2023-01-23 19:20:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          demo CI/CD pipeline using Github Actions.
+          React App.
         </p>
         <a
           className="App-link"
